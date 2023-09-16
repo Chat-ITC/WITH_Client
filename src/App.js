d1b7e217f4e9cf351ad9b0fb9eb7d5018453d694
[Si Hoon] <[dlatlgns000@gmail.com>
2023-09-16 14:54:41 +0900

    다섯번째 커밋

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        야성아!!!!!!!
-      </header>
-=======
-      하이~
->>>>>>> 4b4ac7a853214a80d71281ac095654a86fb4b916
+        야성아!!!!!!! 병합해줘!!!
     </div>
   );
 }
