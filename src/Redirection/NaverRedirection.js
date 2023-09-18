f45f230fb382229e5f9d1795b4bc5cfa1691f25e
[Si Hoon] <[dlatlgns000@gmail.com>
2023-09-18 19:03:46 +0900

    에러메세지11

--- src/Redirection/NaverRedirection.js
+++ src/Redirection/NaverRedirection.js
@@ -21,10 +21,34 @@ const NaverRedirection = () => {
 
   if (loading) return <p>로그인 중입니다. 잠시만 기다려주세요.</p>;
   if (error) {
-    const data = error.response?.data;
-    console.log(data);
-    console.log({ state: { data: data } });
-    navigate("/AddInfoPage", { state: { data: data } });
+    const data = error.response.data;
+    const statusCode = error.response.status;
+    const errorMessage = error.response.data.message;
+    const errorHeaders = error.response.headers;
+    
+
+    if (statusCode === 401) {
+      // 400 상태 코드 처리
+      if (errorMessage === 'there is no refreshToken in redis') {
+        alert('세션이 만료되었습니다. 다시 로그인해 주세요')
+        navigate("/login");
+      } else if (errorMessage === 'your token has been expired') {
+        console.log("토큰 재발급 필요");
+        console.error('토큰 재발급 필요');
+      }
+    }
+    else if (statusCode === 404) {
+      console.log("404에러");
+      console.log(errorHeaders.validation);
+      if (errorHeaders.validation === 'no') {
+        console.log("No Account");
+        navigate("/AddInfoPage", { state: { data: data } });
+      }
+    }
+    else if (statusCode === 409) {
+      alert('세션이 만료되었습니다. 다시 로그인해 주세요')
+      navigate("/login");
+    }
   }
   if (data) {
     const accessToken = data.headers["accesstoken"];
