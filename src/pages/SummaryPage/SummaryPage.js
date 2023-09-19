528c719017fde581b60c2232b465cd06e3a290a8
SiHoon61 <dlatlgns000@gmail.com>
2023-09-19 22:45:56 +0900

    summary페이지에 모달 추가

--- src/pages/SummaryPage/SummaryPage.js
+++ src/pages/SummaryPage/SummaryPage.js
@@ -3,12 +3,32 @@ import Back from "../../assets/AddInfoIcons/back.png";
 import Scrab from "../../assets/icons/clip.png";
 import Copy from "../../assets/AddInfoIcons/Copy.png";
 import Careful from "../../assets/AddInfoIcons/Becareful.png";
+import Becareful from "../../assets/AddInfoIcons/Becareful.png";
+
 
 import Bottom from "../../component/Bottom/Bottom";
 import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from "react";
+//modal
+import SelectModal from "../../Modal/SelectModal/SelectModal";
+import SubModal from "../../Modal/Subject/Subject";
+import LangModal from "../../Modal/LangModal/LangModal";
 
 const SummaryPage = () => {
+  const [isModalOpen, sestIsModalOpen] = useState(false);
+  const openModal = () => sestIsModalOpen(true);
+  const closeModal = () => sestIsModalOpen(false);
+
+  //학습 언어
+  const [isModalOpen1, sestIsModalOpen1] = useState(false);
+  const openModal1 = () => sestIsModalOpen1(true);
+  const closeModal1 = () => sestIsModalOpen1(false);
+
+  //주제
+  const [isModalOpen2, sestIsModalOpen2] = useState(false);
+  const openModal2 = () => sestIsModalOpen2(true);
+  const closeModal2 = () => sestIsModalOpen2(false);
+
   const [file, setFile] = useState(null);
 
   const location = useLocation();
@@ -59,6 +79,68 @@ const SummaryPage = () => {
           </button>
         </div>
       </footer>
+      <div className={styles.HomeMainModal}>
+        <button type="button" onClick={openModal}>
+          open
+        </button>
+        <SelectModal isOpen={isModalOpen} closeModal={closeModal}>
+          <div className={styles.SelectUp}>
+            <em className={styles.SelectLang}>학습언어</em>
+            <button
+              className={styles.SelectLanguage}
+              type="button"
+              onClick={openModal1}
+            >
+              C언어
+            </button>
+          </div>
+          <div className={styles.SelectDown}>
+            <span className={styles.SelectSubj}>
+              <em className={styles.SelectSub}>주제</em>
+              <img src={Becareful} alt="경고" />
+            </span>
+            <button
+              className={styles.SelectSubject}
+              type="button"
+              onClick={openModal2}
+            >
+              선택없음
+            </button>
+          </div>
+          <div className={styles.SelectBottom}>
+            <button
+              className={styles.SelectBtn}
+              type="button"
+              onClick={openModal}
+            >
+              확인
+            </button>
+            <button
+              onClick={closeModal}
+              className={styles.SelectBtn}
+              type="button"
+            >
+              취소
+            </button>
+          </div>
+        </SelectModal>
+      </div>
+      <div
+        className={`${styles.HomePage_Lang} ${isModalOpen1 ? styles.modal_open : ""
+          }`}
+        style={{ display: isModalOpen1 ? "block" : "none" }}
+      >
+        <LangModal isOpen={isModalOpen1} onClose={closeModal1}></LangModal>
+      </div>
+      <Bottom />
+      {/*주제 모달*/}
+      <div
+        className={`${styles.HomePage_Sub} ${isModalOpen2 ? styles.modal_open : ""
+          }`}
+        style={{ display: isModalOpen2 ? "block" : "none" }}
+      >
+        <SubModal isOpen={isModalOpen2} onClose={closeModal2}></SubModal>
+      </div>
       <Bottom />
     </>
   );
