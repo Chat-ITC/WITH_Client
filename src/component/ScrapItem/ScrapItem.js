053df693ecaa607ed9612866add7be33f663e29d
Ji_Sung05 <dlwltjd6847@gmail.com>
2023-09-19 22:38:09 +0900

    ScrapItem Create

--- src/component/ScrapItem/ScrapItem.js
+++ src/component/ScrapItem/ScrapItem.js
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 import styles from "./ScrapItem.module.css";
 
 const ScrapItem = () => {
   const [scrapData, setScrapData] = useState(null);
 
+  //useEffect 훅을 사용하여 컴포넌트가 마운트 되었을 때 백엔드 API를 호출하는 로직
   useEffect(() => {
     // 백엔드 API 호출
-    fetch("백엔드 API 주소")
-      .then((response) => response.json())
-      .then((data) => setScrapData(data))
+    axios
+      .get("백엔드 API 주소")
+      .then((response) => setScrapData(response.data))
       .catch((error) => console.error(error));
   }, []);
 
@@ -16,13 +18,18 @@ const ScrapItem = () => {
     return <div>Loading...</div>;
   }
 
-  const { title, content, timestamp } = scrapData;
+  const { title, content, Language } = scrapData;
+
+  const handleClick = () => {
+    // 다음 창으로 넘어가는 동작 수행
+    window.location.href = "다음 창 주소";
+  };
 
   return (
-    <div className={styles.ScrapItem}>
-      <h2 className={styles.Title}>{styles.title}</h2>
-      <p className={styles.Content}>{styles.content}</p>
-      <p className={styles.Timestamp}>올린 시간: {styles.timestamp}</p>
+    <div className={styles.ScrapItem} onClick={handleClick}>
+      <h2 className={styles.Title}>{title}</h2>
+      <p className={styles.Content}>{content}</p>
+      <p className={styles.Language}>{Language}</p>
     </div>
   );
 };
