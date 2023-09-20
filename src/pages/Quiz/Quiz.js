85b456b8c7eead75f6cf5ffc24cdcbbc952afade
Ji_Sung05 <dlwltjd6847@gmail.com>
2023-09-20 19:00:38 +0900

    Header fixed

--- src/pages/Quiz/Quiz.js
+++ src/pages/Quiz/Quiz.js
@@ -1,8 +1,10 @@
 import Bottom from "../../component/Bottom/Bottom";
+import styles from "./Quiz.module.css";
 
 const Quiz = () => {
   return (
     <>
+      <header className={styles.Quiz_header}>초보자를 위한 문제</header>
       <Bottom />
     </>
   );
