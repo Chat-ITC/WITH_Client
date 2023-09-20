7c789c007da9d693a7b53cbf9145438e2351022a
Ji_Sung05 <dlwltjd6847@gmail.com>
2023-09-21 00:52:41 +0900

    스크랩, 최근 본 자료 뒤로가기 버튼 만들기

--- src/pages/ScrapPage/ScrapPage.js
+++ src/pages/ScrapPage/ScrapPage.js
@@ -3,14 +3,22 @@ import styles from "./ScrapPage.module.css";
 //component
 import Bottom from "../../component/Bottom/Bottom";
 import ScrapItem from "../../component/ScrapItem/ScrapItem";
+//png
+import Back from "../../assets/AddInfoIcons/back.png";
 
 //library
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const ScrapPage = () => {
   return (
     <>
-      <header className={styles.ScrapHeader}>스크랩</header>
+      <header className={styles.ScrapHeader}>
+        <Link className={styles.BackBtn} type="button" to="/MyPage">
+          <img src={Back} alt="뒤로가기" />
+        </Link>
+        <h1 className={styles.ScrapTitle}>스크랩</h1>
+      </header>
       <Bottom />
     </>
   );
