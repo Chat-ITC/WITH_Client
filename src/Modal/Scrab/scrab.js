d1e7c27bef21d23af38fef8258e8ffc19bf304b9
Ji_Sung05 <dlwltjd6847@gmail.com>
2023-09-21 01:33:02 +0900

    퀴즈, 스크랩, 최근 본 내역 헤더 수정

--- src/Modal/Scrab/scrab.js
+++ src/Modal/Scrab/scrab.js
@@ -3,25 +3,13 @@ import styles from "./scrab.module.css";
 // Library
 import React, { useState } from "react";
 
-const ScrabModal = (isOpen, isClose) => {
-  const [isModalOpen, sestIsModalOpen] = useState(null);
-  const closeModal = () => sestIsModalOpen(false);
-
+function Scrap(isOpen, closeModal) {
   return (
-    <>
-      <div
-        style={{ display: isOpen ? "block" : "none" }}
-        className={styles.ScrapText}
-      >
-        <button className={styles.ScrapBtn} type="button" onClick={isClose}>
-          스크랩
-        </button>
-        <button className={styles.ScrapBtn} type="button" onClick={isClose}>
-          최근 본 내역
-        </button>
-      </div>
-    </>
+    <div style={{ display: isOpen ? "block" : "none" }}>
+      <p>Modal</p>
+      <button onClick={closeModal}>close</button>
+    </div>
   );
-};
+}
 
-export default ScrabModal;
+export default Scrap;
--- src/pages/Quiz/Quiz.js
+++ src/pages/Quiz/Quiz.js
@@ -1,10 +1,21 @@
 import Bottom from "../../component/Bottom/Bottom";
 import styles from "./Quiz.module.css";
+//png
+import Back from "../../assets/AddInfoIcons/back.png";
 
-const Quiz = () => {
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Quiz = (props) => {
+  const { quizTarget } = props;
+
   return (
     <>
-      <header className={styles.Quiz_header}>초보자를 위한 문제</header>
+      <header className={styles.Quiz_header}>
+        <Link className={styles.BackBtn} type="button" to="/MyPage">
+          <img src={Back} alt="뒤로가기" />
+        </Link>
+        <h1 className={styles.BackTitle}>{quizTarget}를 위한 문제</h1>
+      </header>
       <Bottom />
     </>
   );
