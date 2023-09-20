f64b0a7b797df877429a5f3db7d99cefb0ff25fc
Ji_Sung05 <dlwltjd6847@gmail.com>
2023-09-21 03:00:31 +0900

    스크랩 커밋

--- src/Modal/Scrab/scrab.js
+++ src/Modal/Scrab/scrab.js
@@ -3,12 +3,8 @@ import styles from "./scrab.module.css";
 // Library
 import React, { useState } from "react";
 
-function Scrap(isOpen, children, closeModal) {
-  return (
-    <div style={{ display: isOpen ? "block" : "none" }}>
-      <div>{children}</div>
-    </div>
-  );
+function Scrap(isOpen, closeModal) {
+  return <div style={{ display: isOpen ? "block" : "none" }}></div>;
 }
 
 export default Scrap;
