4b6ac85f13fa782d15de6b15b0f98e2e741f66ea
Ji_Sung05 <dlwltjd6847@gmail.com>
2023-09-22 03:32:01 +0900

    써머리

--- src/pages/SummaryPage/SummaryPage.js
+++ src/pages/SummaryPage/SummaryPage.js
@@ -1,54 +1,37 @@
-import styles from "./SummaryPage.module.css";
-import Back from "../../assets/AddInfoIcons/back.png";
-import Copy from "../../assets/AddInfoIcons/Copy.png";
-import Becareful from "../../assets/AddInfoIcons/Becareful.png";
-
-import Star from "../../assets/icons/clip.png";
-import FillStar from "../../assets/AddInfoIcons/FillStar.png";
-
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import Bottom from "../../component/Bottom/Bottom";
 import { useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
-import DynamicCodeBlock from "../../component/DynamicCodeBlock/DynamicCodeBlock";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { useRef } from "react";
-//modal
 import SelectModal from "../../Modal/SelectModal/SelectModal";
 import LangModal from "../../Modal/LangModal/LangModal";
 import SubModal from "../../Modal/Subject/Subject";
 
+import styles from "./SummaryPage.module.css";
+import Back from "../../assets/AddInfoIcons/back.png";
+import Copy from "../../assets/AddInfoIcons/Copy.png";
+import Becareful from "../../assets/AddInfoIcons/Becareful.png";
+import Star from "../../assets/icons/clip.png";
+import FillStar from "../../assets/AddInfoIcons/FillStar.png";
+
 const SummaryPage = () => {
   axios.defaults.withCredentials = true;
 
-  const [isModalOpen, sestIsModalOpen] = useState(false);
-  const [isModalOpen1, sestIsModalOpen1] = useState(false);
-  const [isModalOpen2, sestIsModalOpen2] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen1, setIsModalOpen1] = useState(false);
+  const [isModalOpen2, setIsModalOpen2] = useState(false);
 
-  const closeModal = () => sestIsModalOpen(false);
-
-  const openModal1 = () => {
-    sestIsModalOpen1(true);
-    sestIsModalOpen2(false);
-  };
-
+  const closeModal = () => setIsModalOpen(false);
+  const openModal1 = () => setIsModalOpen1(true);
   const closeModal1 = (lanPara) => {
     setLanguage(lanPara);
+    setIsModalOpen1(false);
+  };
+  const openModal2 = () => setIsModalOpen2(true);
+  const closeModal2 = (quePara) => {
+    setQuestion(sum(quePara));
+    setPrintQuestion(quePara);
+    setIsModalOpen2(false);
   };
 
-  const openModal2 = () => {
-    sestIsModalOpen2(true);
-    sestIsModalOpen1(false);
-  };
-
-  //선택
-
-  const openModal = () => sestIsModalOpen(true);
-
-  //학습 언어
-
-  //전송할 글 설정
   const sum = (quePara) => {
     switch (quePara) {
       case "상관없음":
@@ -67,115 +50,83 @@ const SummaryPage = () => {
         return "알 수 없는 옵션입니다.";
     }
   };
-  //주제
 
-  const closeModal2 = (quePara) => {
-    setQuestion(sum(quePara));
-    setPrintQuestion(quePara);
-    console.log(quePara);
-    console.log(question);
-    console.log(printQuestion);
-  };
-
-  //주제와 언어
   const [printQuestion, setPrintQuestion] = useState("상관없음");
   const [question, setQuestion] = useState("상관없음");
   const [language, setLanguage] = useState("없음");
   const [file, setFile] = useState(null);
-
-  //내용과 코드
   const [data, setData] = useState(null);
-
   const [scrapId, setScrapId] = useState(null);
   const [scrapCheck, setScrapCheck] = useState(0);
   const [isScrappedChange, setIsScrappedChange] = useState("NO");
-
-  //위치 저장
   const [lastLocation, setLastLocation] = useState(null);
 
   const sendDataHandle = async () => {
-    console.log(question);
-    console.log(language);
     closeModal();
     const formData = new FormData();
-
     formData.append("imageFile", file);
     formData.append("question", question);
     formData.append("fav_language", language);
-    console.log("file: ", file);
-    console.log("question: ", question);
-    console.log("language: ", language);
 
     const accessToken = localStorage.getItem("accessToken");
-    console.log("토큰 확인: ", accessToken);
 
-    await axios
-      .post(`${process.env.REACT_APP_SERVER_URL}/ai/summary`, formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-          "Access-Control-Allow-Origin": "*",
-          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-        },
-        timeout: 60000,
-      })
-      .then((response) => {
-        console.log("요청성공");
-        console.log(response.data);
-        setData(response.data);
-        setCodeBlock(extractCodeBlock(response.data.content));
-        console.log("같은지 비교하기. ", response.data.content);
-        setScrapId(response.data.id);
-      })
-      .catch((error) => {
-        console.log("요청실패");
-        console.log(error);
-      });
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_SERVER_URL}/ai/summary`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            "Access-Control-Allow-Origin": "*",
+            Authorization: `Bearer ${accessToken}`,
+          },
+          timeout: 60000,
+        }
+      );
+      setData(response.data);
+      setCodeBlock(extractCodeBlock(response.data.content));
+      setScrapId(response.data.id);
+    } catch (error) {
+      console.log("요청실패", error);
+    }
   };
 
   const location = useLocation();
   useEffect(() => {
     setFile(location.state.file);
     setLastLocation(location.state.location);
-    openModal();
+    setIsModalOpen(true);
   }, [location.state.file]);
 
-  //코드 블럭 추출 로직
   function extractCodeBlock(content) {
-    // 정규식을 사용하여 코드 블록을 추출합니다.
     const codeBlock = content.match(/```([\s\S]*?)```/);
-
     return codeBlock ? codeBlock[0] : null;
   }
 
   const [codeBlock, setCodeBlock] = useState(null);
 
-  //코드 블럭 스타일
   const codeBlockStyle = {
-    backgroundColor: "#000000", // 배경색 변경
+    backgroundColor: "#000000",
     padding: "10px",
-    borderRadius: "4px", // 모서리 둥글게 만들기
+    borderRadius: "4px",
     overflow: "auto",
   };
 
   const contentStyle = {
-    color: "white", // 글자 색 변경
-    letterspacing: "20px",
+    color: "white",
+    letterSpacing: "20px",
   };
 
   const preWrap = {
-    whiteSpace: "pre-wrap", // 공백 문자와 줄 바꿈 보존
+    whiteSpace: "pre-wrap",
   };
 
-  //스크랩버튼
   const scrapHandle = () => {
-    console.log("버튼이 클릭되었습니다!");
-
     const formDataId = new FormData();
     formDataId.append("id", scrapId);
-    console.log("scrapId: ", scrapId);
 
     try {
-      const response = axios.post(
+      axios.post(
         `${process.env.REACT_APP_SERVER_URL}/ai/summary/like`,
         formDataId,
         {
@@ -186,9 +137,7 @@ const SummaryPage = () => {
           },
         }
       );
-      console.log("응답 성공:", response);
       setScrapCheck(scrapCheck + 1);
-      console.log(scrapCheck);
       if (scrapCheck % 2 === 0) {
         alert("스크랩 완료");
         setIsScrappedChange("YES");
@@ -197,12 +146,10 @@ const SummaryPage = () => {
         setIsScrappedChange("NO");
       }
     } catch (error) {
-      console.log(error);
-      console.log("요청실패");
+      console.log("요청실패", error);
     }
   };
 
-  //복사버튼
   const copyHandle = () => {
     navigator.clipboard
       .writeText(codeBlock)
@@ -248,7 +195,6 @@ const SummaryPage = () => {
           <div className={styles.HomeMainModal}>
             <div className={styles.SelectPart}>
               <strong className={styles.SelectLang}>학습언어</strong>
-              {/* 모달1가 열림*/}
               <button
                 className={styles.SelectLanguage}
                 type="button"
@@ -261,7 +207,6 @@ const SummaryPage = () => {
               <span className={styles.SelectSubj}>
                 <strong className={styles.SelectSub}>주제</strong>
               </span>
-              {/* 모달2가 열림*/}
               <button
                 className={styles.SelectSubject}
                 type="button"
@@ -271,7 +216,6 @@ const SummaryPage = () => {
               </button>
             </div>
             <div className={styles.SelectBottom}>
-              {/* 데이터 전송 버튼 */}
               <button
                 className={styles.SelectBtn}
                 type="button"
@@ -279,20 +223,17 @@ const SummaryPage = () => {
               >
                 확인
               </button>
-              {/* SelectModal창 닫기 버튼 */}
               <Link to="/HomePage" className={styles.SelectBtn}>
                 취소
               </Link>
             </div>
           </div>
-          {/*주제 모달*/}
           <div
             className={styles.HomePage_Sub}
             style={{ display: isModalOpen2 ? "block" : "none" }}
           >
             <SubModal isOpen={isModalOpen2} onClose={closeModal2} />
           </div>
-          <Bottom />
         </SelectModal>
         {data ? (
           <div style={preWrap}>
@@ -317,25 +258,20 @@ const SummaryPage = () => {
           ""
         )}
       </article>
-      {/* <article className={styles.article}>
-        <button type="button">
-          <img className={styles.Copy} src={Copy} alt="복사" />
-        </button>
-      </article> */}
       <footer>
         <div className={styles.articleDesc}>
           <img className={styles.articleImg} src={Becareful} alt="주의" />
           <span>내용이 정확하지 않을 수 있습니다</span>
         </div>
       </footer>
-      {/* 학습언어 */}
       <div
         className={styles.HomePage_Lang}
-        style={{ display: openModal1 ? "block" : "none" }}
+        style={{ display: isModalOpen1 ? "block" : "none" }}
       >
-        <LangModal isOpen={openModal1} onClose={closeModal1} />
+        <LangModal isOpen={isModalOpen1} onClose={closeModal1} />
       </div>
     </>
   );
 };
+
 export default SummaryPage;
