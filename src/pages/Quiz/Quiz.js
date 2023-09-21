35fe8682cf0592e61cbbfc5f93e692c5634baf15
SiHoon61 <dlatlgns000@gmail.com>
2023-09-21 22:38:04 +0900

    퀴즈 추가

--- src/pages/Quiz/Quiz.js
+++ src/pages/Quiz/Quiz.js
@@ -1,15 +1,103 @@
 import Bottom from "../../component/Bottom/Bottom";
 import styles from "./Quiz.module.css";
 //png
-import Back from "../../assets/AddInfoIcons/back.png";
 
+import ScrapItem from "../../component/ScrapItem/ScrapItem";
+import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Quiz = (props) => {
-  const { quizTarget } = props;
+  const { quizTarget } = props; //유저 실력 넣기
+
+  const navigate = useNavigate();
+
+  const [quizData, setQuizData] = useState([]);
+
+  const roadQuiz = async () => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+    } else {
+      axios.defaults.headers.common["Authorization"] = null;
+    }
+
+    const response = await axios.get(
+      `${process.env.REACT_APP_SERVER_URL}/question/get/language`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return response;
+  };
+
+  useEffect(() => {
+    // roadQuiz 함수를 호출하고 데이터를 받아옵니다.
+    roadQuiz()
+      .then((response) => {
+        //map으로 배열 자체 넘기기
+         console.log(response);
+         console.log(response.data);
+         console.log(response.data[0]);
+        
+        setQuizData(response.data);
+        historyClickHandle();
+      })
+      .catch((error) => {
+        const statusCode = error.response.status;
+
+        if (statusCode === 401) {
+          // 400 상태 코드 처리
+          alert("로그인 해주세요");
+          navigate("/");
+        } else if (statusCode === 409) {
+          alert("세션이 만료되었습니다. 다시 로그인해 주세요");
+          navigate("/");
+        }
+      });
+  }, []);
+
+  const handleIdCheck = (contentProp, isScrappedProp, idProp) => {
+    console.log(contentProp);
+    console.log(isScrappedProp);
+    console.log(idProp);
+    navigate("/ListClickSummary", {
+      state: {
+        content: contentProp,
+        isScrapped: isScrappedProp,
+        id: idProp,
+      },
+    });
+  };
 
 
+  const [historyORScrap, setHistoryORScrap] = useState("history");
 
+  const historyClickHandle = () => {
+    setHistoryORScrap("history");
+    roadQuiz()
+      .then((response) => {
+        setQuizData(response.data);
+      })
+      .catch((error) => {
+        const statusCode = error.response.status;
+
+        if (statusCode === 401) {
+          alert("로그인 해주세요");
+          navigate("/");
+        } else if (statusCode === 409) {
+          alert("세션이 만료되었습니다. 다시 로그인해 주세요");
+          navigate("/");
+        }
+      });
+  };
 
 
 
@@ -25,11 +113,44 @@ const Quiz = (props) => {
   return (
     <>
       <header className={styles.Quiz_header}>
-        <Link className={styles.BackBtn} type="button" to="/MyPage">
-          <img src={Back} alt="뒤로가기" />
-        </Link>
         <h1 className={styles.BackTitle}>{quizTarget}를 위한 문제</h1>
       </header>
+
+      <div className={styles.historyList}>
+        <ul>
+          {Array.isArray(quizData) &&
+            quizData.map((dataList, index) => (
+              <li
+                key={index}
+                onClick={() =>
+                  handleIdCheck(
+                    dataList.content,
+                    dataList.isScrapped,
+                    dataList.id
+                  )
+                }
+              >
+                <ScrapItem
+                  key={index}
+                  title={dataList.title} // 수정된 부분
+                  content={dataList.content}
+                  createAt={dataList.createAt}
+                  fav_language={dataList.fav_language}
+                  id={dataList.id}
+                  isScrapped={dataList.isScrapped}
+                />
+              </li>
+            ))}
+        </ul>
+      </div>
+
+
+
+
+
+
+
+
       <Bottom />
     </>
   );
