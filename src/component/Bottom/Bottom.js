f792c643e7931df82f2bee93c17562957d5573ee
Ji_Sung05 <dlwltjd6847@gmail.com>
2023-09-22 22:36:00 +0900

    Bottom 글씨 완성

--- src/component/Bottom/Bottom.js
+++ src/component/Bottom/Bottom.js
@@ -14,104 +14,66 @@ const Bottom = ({ logo }) => {
   const [strQuizClick, setStrQuizClick] = useState(styles.nonclickStr);
   const [imgHomeClick, setImgHomeClick] = useState(styles.nonClickImg);
   const [strHomeClick, setStrHomeClick] = useState(styles.nonclickStr);
-  const [imgMyPageClick, setImgMyPageClick] = useState(styles.nonClickImg);
-  const [strMyPageClick, setStrMyPageClick] = useState(styles.nonclickStr);
+  const [imgMyPageClick, setImgMyPageClick] = useState(styles.nonClickImg1);
+  const [strMyPageClick, setStrMyPageClick] = useState(styles.nonclickStr1);
   console.log(logo);
-
-
-
 
   useEffect(() => {
     switch (logo) {
-      case 'quiz':
+      case "quiz":
         setImgQuizClick(styles.clickImg);
         setStrQuizClick(styles.clickStr);
         setImgHomeClick(styles.nonClickImg);
         setStrHomeClick(styles.nonclickStr);
-        setImgMyPageClick(styles.nonClickImg);
-        setStrMyPageClick(styles.nonclickStr);
+        setImgMyPageClick(styles.nonClickImg1);
+        setStrMyPageClick(styles.nonclickStr1);
         break;
-      case 'home':
+      case "home":
         setImgQuizClick(styles.nonClickImg);
         setStrQuizClick(styles.nonclickStr);
         setImgHomeClick(styles.clickImg);
         setStrHomeClick(styles.clickStr);
-        setImgMyPageClick(styles.nonClickImg);
-        setStrMyPageClick(styles.nonclickStr);
+        setImgMyPageClick(styles.nonClickImg1);
+        setStrMyPageClick(styles.nonclickStr1);
         break;
-      case 'mypage':
+      case "mypage":
         setImgQuizClick(styles.nonClickImg);
         setStrQuizClick(styles.nonclickStr);
         setImgHomeClick(styles.nonClickImg);
         setStrHomeClick(styles.nonclickStr);
-        setImgMyPageClick(styles.clickImg);
-        setStrMyPageClick(styles.clickStr);
+        setImgMyPageClick(styles.clickImg1);
+        setStrMyPageClick(styles.clickStr1);
         break;
       default:
         break;
     }
   }, [logo]); // logo가 변경될 때만 useEffect가 실행됩니다.
 
-
   return (
     <div className={styles.bottomContainer}>
-    <nav>
-      <ul className={styles.BottomInfo}>
-        <Link to="/Quiz">
-          <li className={styles.bottomLink}>
-            <img
-              className={imgQuizClick}
-              src={Quiz}
-              alt="퀴즈"
-            />
-            <div
-              className={strQuizClick}
-
-            >
-              퀴즈
-            </div>
-
-          </li>
-        </Link>
-        <Link to="/HomePage">
-          <li className={styles.bottomLink}>
-
-            <img
-
-              className={imgHomeClick}
-              src={Logo}
-              alt="홈"
-            />
-            <div
-              className={strHomeClick}
-
-            >
-              홈
-            </div>
-          </li>
-        </Link>
-        <Link to="/MyPage">
-          <li className={styles.bottomLink}>
-
-            <img
-
-
-              className={imgMyPageClick}
-              src={MyPage}
-              alt="내 정보"
-            />
-            <div
-              className={strMyPageClick}
-
-            >
-              내 정보
-            </div>
-
-          </li>
-        </Link>
-      </ul>
-    </nav>
-  </div>
-);
+      <nav>
+        <ul className={styles.BottomInfo}>
+          <Link to="/Quiz">
+            <li className={styles.bottomLink}>
+              <img className={imgQuizClick} src={Quiz} alt="퀴즈" />
+              <div className={strQuizClick}>퀴즈</div>
+            </li>
+          </Link>
+          <Link to="/HomePage">
+            <li className={styles.bottomLink}>
+              <img className={imgHomeClick} src={Logo} alt="홈" />
+              <div className={strHomeClick}>홈</div>
+            </li>
+          </Link>
+          <Link to="/MyPage">
+            <li className={styles.bottomLink}>
+              <img className={imgMyPageClick} src={MyPage} alt="내 정보" />
+              <div className={strMyPageClick}>내 정보</div>
+            </li>
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 export default Bottom;
