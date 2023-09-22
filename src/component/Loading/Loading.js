2359238218f3307d694e73eab4eddf4f8b378f6a
SiHoon61 <dlatlgns000@gmail.com>
2023-09-23 04:01:02 +0900

    로딩 수정2

--- src/component/Loading/Loading.js
+++ src/component/Loading/Loading.js
@@ -1,13 +1,12 @@
 // Loading.js
 import React from 'react';
-import {Background,} from './Styles';
 import Spinner from '../../assets/loading/Spinner.gif'
 
 export const Loading = () => {
   return (
-    <Background>
+
       <img src={Spinner} alt = "로딩중" width="20%"/>
-    </Background>
+
   )
 };
 
