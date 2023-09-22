35b0ddd9aaa901754051da0ff24ef03c16b578a8
SiHoon61 <dlatlgns000@gmail.com>
2023-09-23 00:33:27 +0900

    퀴즈 주석

--- src/pages/Quiz/QuizSelect.js
+++ src/pages/Quiz/QuizSelect.js
@@ -2,26 +2,26 @@
 
 
 
-const QuizSelect = () => {
-    const navigate = useNavigate();
-    axios.defaults.withCredentials = true;
-    const location = useLocation();
-    const { answer, content, title } = location.state;
+// const QuizSelect = () => {
+//     const navigate = useNavigate();
+//     axios.defaults.withCredentials = true;
+//     const location = useLocation();
+//     const { answer, content, title } = location.state;
     
-    const [scrapId, setScrapId] = useState(null);
-    const [scrapCheck, setScrapCheck] = useState(0);
-    const [codeBlock, setCodeBlock] = useState(null);
-    const [isScrappedChange, setIsScrappedChange] = useState(isScrapped);
+//     const [scrapId, setScrapId] = useState(null);
+//     const [scrapCheck, setScrapCheck] = useState(0);
+//     const [codeBlock, setCodeBlock] = useState(null);
+//     const [isScrappedChange, setIsScrappedChange] = useState(isScrapped);
 
 
-    return (
-        <>
-            <header className={styles.}>
-                <h1 className={styles.}>{quizLevel}를 위한 문제</h1>
-            </header>
-        </>
-    )
-};
+//     return (
+//         <>
+//             <header className={styles.}>
+//                 <h1 className={styles.}>{quizLevel}를 위한 문제</h1>
+//             </header>
+//         </>
+//     )
+// };
 
 
-export default QuizSelect;
+// export default QuizSelect;
