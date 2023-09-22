9d3289680ac60183bb19018e03556aadafd85c0e
Ji_Sung05 <dlwltjd6847@gmail.com>
2023-09-23 04:55:55 +0900

    하단바 완성

--- src/component/Bottom/Bottom.js
+++ src/component/Bottom/Bottom.js
@@ -1,48 +1,33 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import styles from "./Bottom.module.css";
 
-import Quiz from "../../assets/icons/quiz.png";
-import Logo from "../../assets/AddInfoIcons/NotLofo.png";
-import MyPage from "../../assets/icons/mypage.png";
-import { useEffect } from "react";
-
-import { useState } from "react";
+import Quiz from "../../assets/logo/SelectQuiz.png";
+import Logo from "../../assets/logo/SelectHome.png";
+import MyPage from "../../assets/logo/SelectMyPage.png";
+import React, { useEffect, useState } from "react";
 
 const Bottom = ({ logo }) => {
   const [imgQuizClick, setImgQuizClick] = useState(styles.nonClickImg);
-  const [strQuizClick, setStrQuizClick] = useState(styles.nonclickStr);
   const [imgHomeClick, setImgHomeClick] = useState(styles.nonClickImg);
-  const [strHomeClick, setStrHomeClick] = useState(styles.nonclickStr);
-  const [imgMyPageClick, setImgMyPageClick] = useState(styles.nonClickImg1);
-  const [strMyPageClick, setStrMyPageClick] = useState(styles.nonclickStr1);
+  const [imgMyPageClick, setImgMyPageClick] = useState(styles.nonClickImg);
   console.log(logo);
 
   useEffect(() => {
     switch (logo) {
       case "quiz":
         setImgQuizClick(styles.clickImg);
-        setStrQuizClick(styles.clickStr);
         setImgHomeClick(styles.nonClickImg);
-        setStrHomeClick(styles.nonclickStr);
-        setImgMyPageClick(styles.nonClickImg1);
-        setStrMyPageClick(styles.nonclickStr1);
+        setImgMyPageClick(styles.nonClickImg);
         break;
       case "home":
         setImgQuizClick(styles.nonClickImg);
-        setStrQuizClick(styles.nonclickStr);
         setImgHomeClick(styles.clickImg);
-        setStrHomeClick(styles.clickStr);
-        setImgMyPageClick(styles.nonClickImg1);
-        setStrMyPageClick(styles.nonclickStr1);
+        setImgMyPageClick(styles.nonClickImg);
         break;
       case "mypage":
         setImgQuizClick(styles.nonClickImg);
-        setStrQuizClick(styles.nonclickStr);
         setImgHomeClick(styles.nonClickImg);
-        setStrHomeClick(styles.nonclickStr);
-        setImgMyPageClick(styles.clickImg1);
-        setStrMyPageClick(styles.clickStr1);
+        setImgMyPageClick(styles.clickImg);
         break;
       default:
         break;
@@ -51,28 +36,23 @@ const Bottom = ({ logo }) => {
 
   return (
     <div className={styles.bottomContainer}>
-      <nav>
-        <ul className={styles.BottomInfo}>
-          <Link to="/Quiz">
-            <li className={styles.bottomLink}>
-              <img className={imgQuizClick} src={Quiz} alt="퀴즈" />
-              <div className={strQuizClick}>퀴즈</div>
-            </li>
-          </Link>
-          <Link to="/HomePage">
-            <li className={styles.bottomLink}>
-              <img className={imgHomeClick} src={Logo} alt="홈" />
-              <div className={strHomeClick}>홈</div>
-            </li>
-          </Link>
-          <Link to="/MyPage">
-            <li className={styles.bottomLink}>
-              <img className={imgMyPageClick} src={MyPage} alt="내 정보" />
-              <div className={strMyPageClick}>내 정보</div>
-            </li>
-          </Link>
-        </ul>
-      </nav>
+      <ul className={styles.BottomInfo}>
+        <Link to="/Quiz">
+          <li className={styles.bottomLink}>
+            <img className={imgQuizClick} src={Quiz} alt="퀴즈" />
+          </li>
+        </Link>
+        <Link to="/HomePage">
+          <li className={styles.bottomLink}>
+            <img className={imgHomeClick} src={Logo} alt="홈" />
+          </li>
+        </Link>
+        <Link to="/MyPage">
+          <li className={styles.bottomLink}>
+            <img className={imgMyPageClick} src={MyPage} alt="내 정보" />
+          </li>
+        </Link>
+      </ul>
     </div>
   );
 };
