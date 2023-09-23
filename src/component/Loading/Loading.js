d6720b6090048dfd89b720c17c7291f7614c9e35
Lim Sihoon <dlatlgns000@gmail.com>
2023-09-23 15:57:15 +0900

    로딩 멘트 추가

--- src/component/Loading/Loading.js
+++ src/component/Loading/Loading.js
@@ -4,9 +4,11 @@ import Spinner from '../../assets/loading/Spinner.gif'
 
 export const Loading = () => {
   return (
+    <div>
+      <p>분석을 하는데 20초에서 1분정도 걸릴 수 있습니다.</p>
 
-      <img src={Spinner} alt = "로딩중" width="20%"/>
-
+      <img src={Spinner} alt="로딩중" width="20%" />
+    </div>
   )
 };
 
