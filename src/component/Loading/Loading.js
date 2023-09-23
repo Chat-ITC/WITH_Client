7d21a98d832edb0873e6e7a6d529d861bb88bc9b
Ji_Sung05 <dlwltjd6847@gmail.com>
2023-09-23 16:11:16 +0900

    로딩

--- src/component/Loading/Loading.js
+++ src/component/Loading/Loading.js
@@ -1,15 +1,16 @@
 // Loading.js
-import React from 'react';
-import Spinner from '../../assets/loading/Spinner.gif'
+import React from "react";
+import Spinner from "../../assets/loading/Spinner.gif";
+import styles from "./Loading.module.css";
 
 export const Loading = () => {
   return (
-    <div>
+    <div className={styles.skel}>
       <p>분석을 하는데 20초에서 1분정도 걸릴 수 있습니다.</p>
 
       <img src={Spinner} alt="로딩중" width="20%" />
     </div>
-  )
+  );
 };
 
 export default Loading;
